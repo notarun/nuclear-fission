13aa04578ea0a30c632cbbe41cf9e47ffec43913
Arun S. Negi <arunsnegi@protonmail.com>
2025-04-27 14:17:34 +0530

    feat: add grid in game scene

--- game.lua
+++ game.lua
@@ -1,7 +1,67 @@
-local function GameScene()
-  print("hi")
+local Color = require 'color'
+local useStore = require 'store'
 
-  return {}
+local lg = love.graphics
+
+local function GridCell(x, y, opts)
+  assert(type(opts.value) == "number", "Invalid string `opts.value`")
+  opts.size = opts.size or 80
+
+  local function draw()
+    lg.setColor(Color.White)
+    lg.rectangle("line", x, y, opts.size, opts.size)
+    lg.print(opts.value, x, y)
+  end
+
+  return { draw = draw }
+end
+
+local function Grid(x, y, opts)
+  x, y, opts = x or 0, y or 0, opts or {}
+  opts.rows = opts.rows or 12
+  opts.cols = opts.cols or 6
+  opts.cellSize = opts.cellSize or 80
+
+  local state, actions = useStore()
+  actions.createMatrix(opts.rows, opts.cols)
+
+  local cells = {}
+  for i, cols in ipairs(state.matrix) do
+    for j, value in ipairs(cols) do
+      local cellX = x + (j - 1) * opts.cellSize
+      local cellY = y + (i - 1) * opts.cellSize
+      table.insert(cells, GridCell(cellX, cellY, {
+        value = value,
+        size = opts.cellSize,
+      }))
+    end
+  end
+
+  local function draw()
+    for _, c in ipairs(cells) do c.draw() end
+  end
+
+  return { draw = draw }
+end
+
+local function GameScene()
+  local entities = {
+    Grid(0, 0)
+  }
+
+  local function update(dt)
+    for _, b in ipairs(entities) do
+      if b.update then b.update(dt) end
+    end
+  end
+
+  local function draw()
+    for _, b in ipairs(entities) do
+      if b.draw then b.draw() end
+    end
+  end
+
+  return { update = update, draw = draw }
 end
 
 return GameScene
--- util.lua
+++ util.lua
@@ -1,1 +1,16 @@
-return { noop = function()  end }
+return {
+  noop = function() end,
+
+  createMatrix = function(rows, cols, default)
+    local matrix = {}
+
+    for i = 1, rows do
+      matrix[i] = {}
+      for j = 1, cols do
+        matrix[i][j] = default
+      end
+    end
+
+    return matrix
+  end
+}
