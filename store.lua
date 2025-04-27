0ce4a0f414cde5d72906193c2f8b508096af39ba
Arun S. Negi <arunsnegi@protonmail.com>
2025-04-27 14:16:59 +0530

    feat: add state management

---
+++ store.lua
@@ -0,0 +1,32 @@
+local util = require 'util'
+
+local function State()
+  return {
+    matrix = util.createMatrix(12, 6, 0),
+  }
+end
+
+local function Actions(s)
+  local function dumpState() require('dump')(s) end
+
+  local function createMatrix(row, col)
+    s.matrix = util.createMatrix(row, col, 0)
+  end
+
+  local function updateMatrix(i, j, val)
+    s.matrix[i][j] = val
+  end
+
+  return {
+    dumpState = dumpState,
+    createMatrix = createMatrix,
+    updateMatrix = updateMatrix,
+  }
+end
+
+local state = State()
+local actions = Actions(state)
+
+return function()
+  return state, actions
+end
