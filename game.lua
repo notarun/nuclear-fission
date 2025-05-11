77ae8b409eff0752b4ecc2c9d1a14f6aa20d37c5
Arun S. Negi <arunsnegi@protonmail.com>
2025-05-12 01:47:48 +0530

    feat: add preliminary split animation

--- game.lua
+++ game.lua
@@ -1,49 +1,75 @@
+local flux = require("3rd.flux.flux")
 local lume = require("3rd.lume.lume")
 local toast = require("3rd.toasts.lovelyToasts")
 
-local Color = require("color")
 local core = require("core")
 local drw = require("draw")
 local input = require("input")
 local state = require("state")
 
 local lg, lm = love.graphics, love.mouse
+local sf = string.format
 
-local function Neutron(cellCtx, data)
-  local cell = state.cell(data.self.i, data.self.j)
-  local color, vibeMag, splitting = cell.ownedBy.color, 0, false
+local entities = {}
+
+local function neutronsInCell(i, j)
+  return lume.chain(entities):filter(function(e)
+    local isNeutron = lume.find(e.ctx.tags, "neutron")
+    local inSameCell = lume.find(e.ctx.tags, sf("%s-%s", i, j))
+    return isNeutron and inSameCell
+  end)
+end
+
+local function cellPosAndSz(i, j)
+  local rows, cols = state.matrixDimensions()
+  local vw, vh = lg.getDimensions()
+  local w, h = vw / cols, vh / rows
+  local x, y = (j - 1) * w, (i - 1) * h
+
+  return x, y, w, h
+end
+
+local function Neutron(data)
+  local i, j = data.self.i, data.self.j
+  local cell = state.cell(i, j)
+  local color, vibeMag, goalPos = cell.ownedBy.color, 0, {}
+
+  local dirs = {
+    { { 0, 0 }, { 0, 0 } },
+    { { -10, 0 }, { 10, 0 } },
+    { { -10, -4 }, { 0, 6 }, { 10, -4 } },
+    { { -10, 0 }, { 0, -10 }, { 0, 10 }, { 10, 0 } },
+  }
 
   local function vibrate(mag)
     vibeMag = mag
   end
 
   local function split(pld)
-    require("dump")(pld)
+    local n = pld.neighbors[data.idx]
+    local cx, cy, cw, ch = cellPosAndSz(n.i, n.j)
+    goalPos.x, goalPos.y = cx + cw / 2, cy + ch / 2
+  end
+
+  local function load(ctx)
+    local cx, cy, cw, ch = cellPosAndSz(i, j)
+    ctx.x, ctx.y = cx + cw / 2, cy + ch / 2
   end
 
   local function update(_, ctx)
-    ctx.x, ctx.y = cellCtx.x + cellCtx.w / 2, cellCtx.y + cellCtx.h / 2
+    if goalPos.x and goalPos.y then
+      flux.to(ctx, 0.1, goalPos):ease("linear"):oncomplete(function()
+        ctx.dead = true
+      end)
+    else
+      load(ctx)
 
-    if not splitting then
-      if cell.count == 2 then
-        if data.idx == 1 then
-          ctx.x = ctx.x - 10
-        elseif data.idx == 2 then
-          ctx.x = ctx.x + 10
-        end
-      elseif cell.count == 3 then
-        if data.idx == 1 then
-          ctx.x = ctx.x - 10
-        elseif data.idx == 2 then
-          ctx.y = ctx.y + 10
-        elseif data.idx == 3 then
-          ctx.x = ctx.x + 10
-        end
-        ctx.y = ctx.y - 4
+      local dir = dirs[cell.count] and dirs[cell.count][data.idx]
+      if dir then
+        ctx.x, ctx.y = ctx.x + dir[1], ctx.y + dir[2]
+        color = cell.ownedBy.color
       end
     end
-
-    color = cell.ownedBy and cell.ownedBy.color or Color.White
   end
 
   local function draw(ctx)
@@ -51,33 +77,30 @@ local function Neutron(cellCtx, data)
   end
 
   return core.Entity({
-    update = update,
+    load = load,
     draw = draw,
+    update = update,
+    tags = { "neutron", sf("%s-%s", i, j) },
     events = { vibrate = vibrate, split = split },
   })
 end
 
+local function fuseOrSplit(i, j)
+  state.fuseOrSplit(i, j, state.playing().idx, function(ev, pld)
+    if ev == "add" then
+      lume.push(entities, Neutron(pld))
+    elseif ev == "split" then
+      neutronsInCell(i, j):each("emit", "split", pld)
+    end
+  end)
+end
+
 local function Cell(i, j)
-  local rows, cols = state.matrixDimensions()
-  local neutrons = {}
-
-  local function fuseOrSplit(ctx)
-    state.fuseOrSplit(i, j, state.playing().idx, function(ev, pld)
-      if ev == "add" then
-        lume.push(neutrons, Neutron(ctx, pld))
-      elseif ev == "split" then
-        lume.each(neutrons, "emit", "split", pld)
-      end
-    end)
-  end
-
-  local function update(dt, ctx)
+  local function update(_, ctx)
     local winner = state.winner()
     if winner then core.goToScene("menu", { mode = "result" }) end
 
-    local vw, vh = lg.getDimensions()
-    ctx.w, ctx.h = vw / cols, vh / rows
-    ctx.x, ctx.y = (j - 1) * ctx.w, (i - 1) * ctx.h
+    ctx.x, ctx.y, ctx.w, ctx.h = cellPosAndSz(i, j)
 
     local items = core.world:queryPoint(lm.getPosition())
     local hovering = lume.find(items, ctx.item) ~= nil
@@ -88,49 +111,37 @@ local function Cell(i, j)
       if owner and owner ~= playing then
         toast.show("This cell is owned by other player")
       else
-        fuseOrSplit(ctx)
+        fuseOrSplit(i, j)
         state.nextMove()
       end
-
-      local threshold = #state.cellNeighbors(i, j) - 1
-      local magnitude = state.cell(i, j).count < threshold and 0 or 0.1
-      lume.each(neutrons, "emit", "vibrate", magnitude)
     end
 
-    lume.each(neutrons, "update", dt)
+    local threshold = #state.cellNeighbors(i, j) - 1
+    local magnitude = state.cell(i, j).count < threshold and 0 or 0.1
+    neutronsInCell(i, j):each("emit", "vibrate", magnitude)
   end
 
   local function draw(ctx)
-    local c = state.cell(i, j)
-    local str = string.format("o%s\nc%s", c.owner or "", c.count)
-    lg.setColor(Color.CookiesAndCream)
-    lg.print(str, ctx.x, ctx.y)
-
     lg.setColor(state.playing().player.color)
     lg.rectangle("line", ctx.x, ctx.y, ctx.w, ctx.h)
-    lume.each(neutrons, "draw")
   end
 
   return core.Entity({ update = update, draw = draw })
 end
 
-return (function()
-  local entities = {}
-
-  return core.Scene({
-    id = "game",
-    entities = entities,
-    enter = function()
-      state.init(12, 6, 2)
-      local rows, cols = state.matrixDimensions()
-      for i = 1, rows do
-        for j = 1, cols do
-          lume.push(entities, Cell(i, j))
-        end
+return core.Scene({
+  id = "game",
+  entities = entities,
+  enter = function()
+    state.init(12, 6, 2)
+    local rows, cols = state.matrixDimensions()
+    for i = 1, rows do
+      for j = 1, cols do
+        lume.push(entities, Cell(i, j))
       end
-    end,
-    leave = function()
-      lume.clear(entities)
-    end,
-  })
-end)()
+    end
+  end,
+  leave = function()
+    lume.clear(entities)
+  end,
+})
